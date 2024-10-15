7db3d626d53e034c0ac9769e1148278914e9b0d1
delima.eduardolucas@hotmail.com <delima.eduardolucas@hotmail.com>
2024-10-14 21:14:45 -0300

    controllers

---
+++ Controllers/CategoriaController.cs
@@ -0,0 +1,56 @@
+﻿using BuscaVeiculosAPI.Context;
+using BuscaVeiculosAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuscaVeiculosAPI.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class CategoriaController : ControllerBase
+{
+
+    private readonly AppDbContext _context;
+
+    public CategoriaController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Categoria>> Get()
+    {
+        var categorias = _context.Categorias.ToList();
+        if(categorias is null) 
+        {
+            return NotFound("Categoria não encontrada.");
+        }
+        return categorias;
+    }
+
+    [HttpGet("{id:int}", Name="ObterCategoria")]
+    public ActionResult<Categoria> Get(int id)
+    {
+        var categoria = _context.Categorias.FirstOrDefault(c => c.Id == id);
+        if ( categoria is null)
+        {
+            return NotFound("Categoria não encontrada.");
+        }
+        return categoria;
+    }
+
+    [HttpPost]
+    public ActionResult Post(Categoria categoria)
+    {
+        if (categoria is null)
+            return BadRequest();
+
+        _context.Categorias.Add(categoria);
+        _context.SaveChanges();
+
+        return new CreatedAtRouteResult("ObterCategoria",
+            new { id = categoria.Id }, categoria);
+    }
+
+
+}
---
+++ Controllers/VeiculoController.cs
@@ -0,0 +1,55 @@
+﻿using BuscaVeiculosAPI.Context;
+using BuscaVeiculosAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuscaVeiculosAPI.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class VeiculoController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public VeiculoController(AppDbContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    public ActionResult<IEnumerable<Veiculo>> Get()
+    {
+        var veiculos = _context.Veiculos.ToList();
+        if (veiculos is null)
+        {
+            return NotFound("Veiculo não encontrado.");
+        }
+        return veiculos;
+    }
+
+    [HttpGet("{id:int}", Name = "ObterVeiculo")]
+    public ActionResult<Veiculo> Get(int id)
+    {
+        var veiculo = _context.Veiculos.FirstOrDefault(c => c.Id == id);
+        if (veiculo is null)
+        {
+            return NotFound("Veiculo não encontrado.");
+        }
+        return veiculo;
+    }
+
+    [HttpPost]
+    public ActionResult Post(Veiculo veiculo)
+    {
+        if (veiculo is null)
+            return BadRequest();
+
+        _context.Veiculos.Add(veiculo);
+        _context.SaveChanges();
+
+        return new CreatedAtRouteResult("ObterVeiculo",
+            new { id = veiculo.Id }, veiculo);
+    }
+
+}
+
+
